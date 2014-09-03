1baf0114538ca900dd976223e782aa122b797308
dohar.silalahi@ideation816.com <dohar.silalahi@ideation816.com>
2014-09-02 23:34:09 -0400

    added iptables

--- firewall.sh
+++ firewall.sh
@@ -4,3 +4,4 @@ sudo service iptables stop
 # punch holes on port 80,443
 iptables -A RH-Firewall-1-INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 iptables -A RH-Firewall-1-INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
+
