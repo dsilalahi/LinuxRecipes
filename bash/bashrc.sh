02a70f3df1b2e66e2f8643694e04e4726336381c
Dohar Silalahi <dohar.silalahi@gmail.com>
2018-06-14 11:51:33 -0400

    Create bashrc.sh

---
+++ bash/bashrc.sh
@@ -0,0 +1,8 @@
+export JAVA_HOME="/local/java/jdk1.8.0_121/"
+PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
+export PATH
+
+HISTSIZE=5000
+HISTFILESIZE=10000
+shopt -s histappend
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
